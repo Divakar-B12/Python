3bdc9f96d81a2dc6325007aba81c757f07e9ae0f
Divakar B <bdivakar0786@gmail.com>
2025-02-01 08:53:08 +0530

    Update Practice_2.py

--- Python/Class 1/Practice programs/Practice_2.py
+++ Python/Class 1/Practice programs/Practice_2.py
@@ -1,5 +1,7 @@
 # Install an external module and use it to perform an operation of your interest. 
 
+# pip install pyttsx 
+
 import pyttsx3
 engine = pyttsx3.init()
 engine.say("My Name is Divakar")
