b325ca5fd5230475c5ab3527e116069f0f553819
Divakar B <bdivakar0786@gmail.com>
2025-02-01 08:50:52 +0530

    Update module.py

--- Python/Class 1/module.py
+++ Python/Class 1/module.py
@@ -1,12 +1,16 @@
+# What is Module : A module is a file containing code written by somebody else (usually) which can be imported and used in our programs.
+
 import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+#COMMENTS
 #This print random jokes ---> (Single line comment)
+
 """
 This is 
 a 
 multi line 
 comment
-
-
 """
-joke = pyjokes.get_joke()
-print(joke)
